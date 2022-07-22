4fec6080de984242e60093427e75c6596e7f9fa9
Reymark Rodriguez <reymark.rodriguez@emerson.com>
2022-07-23 00:18:49 +0800

    Initial push from local repo

---
+++ FileUploader/Controllers/FileUpload.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace FileUploader.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FileUpload : Controller
+    {
+        [HttpPost, DisableRequestSizeLimit]
+        public IActionResult Upload()
+        {
+            try
+            {
+                var file = Request.Form.Files[0];
+                var folderName = Path.Combine("Resources", "Images");
+                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+
+                if (file.Length > 0)
+                {
+                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                    var fullPath = Path.Combine(folderName, fileName);
+
+                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    {
+
+                        file.CopyTo(stream);
+                    }
+
+                    return Ok();
+                }
+                else
+                {
+
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, $"Error: {ex} ");
+            }
+        }
+
+    }
+}
